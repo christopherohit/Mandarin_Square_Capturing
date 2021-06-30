d407704ebb34d33b686cb6a0bf7a6292cdd52219
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-06-30 18:09:00 +0700

    This Module Process For Run Application

---
+++ main.py
@@ -0,0 +1,6 @@
+from GamePlay import GamePlay
+
+if __name__ == "__main__":
+    print("+-------- Mandarin_Square_Capturing-----------+")
+    game = GamePlay(algo_0=None , algo_1="expectimax")
+    game.run()
